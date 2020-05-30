e1997eb80125a2763cc10c9ec3471dd483398c3a
Jordy Shryock <jordy1611@gmail.com>
2020-05-30 17:22:52 -0600

    Apply default and active toggle to all buttons

--- main.js
+++ main.js
@@ -1,43 +1,12 @@
-// var ageInput = document.getElementById("age")
-//
-// ageInput.addEventListener("keydown", function(e) {
+// var minutesInput = document.querySelector(".minutes-input");
+// minutesInput.addEventListener("keydown", noLetters);
+// function noLetters(e) {
 //   // prevent: "e", "=", ",", "-", "."
 //   if ([69, 187, 188, 189, 190].includes(e.keyCode)) {
 //     e.preventDefault();
 //   }
-// })  prevent e code for later
+// }) // prevent e code for later
 
-// var studyButton = document.querySelector(".study-button");
-// var meditateButton = document.querySelector(".meditate-button");
-// var exerciseButton = document.querySelector(".exercise-button");
-//
-// studyButton.addEventListener('click', changeStudyButton);
-// meditateButton.addEventListener('click', changeMeditateButton);
-// exerciseButton.addEventListener('click', changeExerciseButton);
-//
-//   event.preventDefault();
-// function changeStudyButton(event) {
-//   studyButton = event.target
-//   studyButton.src = 'assets/study-active.svg';
-//   studyButton.style.color = '#B3FD78';
-//   studyButton.style.borderColor = '#B3FD78';
-// }
-//
-// function changeMeditateButton(event) {
-//   event.preventDefault();
-//   meditateButton = event.target.closest(".meditate-button")
-//   meditateButton.src = "assets/meditate-active.svg"
-//   meditateButton.style.color = "#C278FD";
-//   meditateButton.style.borderColor = "#C278FD";
-// }
-//
-// function changeExerciseButton() {
-//   event.preventDefault();
-//   exerciseButton = event.target
-//   exerciseButton.src = 'assets/exercise-active.svg';
-//   exerciseButton.style.color = '#FD8078';
-//   exerciseButton.style.borderColor = '#FD8078';
-// }
 var studyButtons = document.querySelector(".activity-buttons");
 
 studyButtons.addEventListener("click", selectActivity);
@@ -52,16 +21,52 @@ function selectActivity(event) {
   var exerciseImage = document.querySelector(".exercise-img");
 
   if (event.target.id === "study-button") {
-    studyButton.classList.remove("btn-default"); // create incompassing function for all
-    studyButton.classList.add("study-selected"); // lines in if staement
-    studyImage.src = "assets/study-active.svg";
+    studySelect(studyButton, studyImage);
+    meditateDefault(meditateButton, meditateImage);
+    exerciseDefault(exerciseButton, exerciseImage);
   } else if (event.target.id === "meditate-button") {
-    meditateButton.classList.remove("btn-default"); // create incompassing function for all
-    meditateButton.classList.add("meditate-selected"); // lines in if staement
-    meditateImage.src = "assets/meditate-active.svg";
+    meditateSelect(meditateButton, meditateImage);
+    studyDefault(studyButton, studyImage);
+    exerciseDefault(exerciseButton, exerciseImage);
   } else if (event.target.id === "exercise-button") {
-    exerciseButton.classList.remove("btn-default"); // create incompassing function for all
-    exerciseButton.classList.add("exercise-selected"); // lines in if staement
-    exerciseImage.src = "assets/exercise-active.svg";
+    exerciseSelect(exerciseButton, exerciseImage);
+    studyDefault(studyButton, studyImage);
+    meditateDefault(meditateButton, meditateImage);
   }
+}
+
+function studySelect(button, image) {
+  button.classList.remove("btn-default");
+  button.classList.add("study-selected");
+  image.src = "assets/study-active.svg";
+}
+
+function meditateSelect(button, image) {
+  button.classList.remove("btn-default");
+  button.classList.add("meditate-selected");
+  image.src = "assets/meditate-active.svg";
+}
+
+function exerciseSelect(button, image) {
+  button.classList.remove("btn-default");
+  button.classList.add("exercise-selected");
+  image.src = "assets/exercise-active.svg";
+}
+
+function studyDefault(button, image) {
+  button.classList.add("btn-default");
+  button.classList.remove("study-selected");
+  image.src = "assets/study.svg";
+}
+
+function meditateDefault(button, image) {
+  button.classList.add("btn-default");
+  button.classList.remove("meditate-selected");
+  image.src = "assets/meditate.svg";
+}
+
+function exerciseDefault(button, image) {
+  button.classList.add("btn-default");
+  button.classList.remove("exercise-selected");
+  image.src = "assets/exercise.svg";
 }
