e6fffac1a830bb6b9fdcb34adf426212640c2669
Jordy Shryock <jordy1611@gmail.com>
2020-06-01 21:57:14 -0600

    Start activity countdown functionality

--- activity.js
+++ activity.js
@@ -9,8 +9,15 @@ class Activity {
   }
 
   countdown() {
-    // handles activity timer
-    // uses this.minutes and this.seconds
+    setInterval(function() {
+      var timeDisplay = document.querySelector('.timer');
+      var totalSeconds = (this.minutes * 60) + this.seconds;
+      var activeMinutes = Math.floor((totalSeconds)/60) % 60;
+      var activeSeconds = totalSeconds % 60;
+      for (var i = total.seconds; i = 0; i--) {
+        console.log()
+      }
+    }, 1000)
   }
 
   markComplete() {
